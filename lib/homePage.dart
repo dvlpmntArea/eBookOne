ff9021f6ca853b0717fc580d3fdc0704dc43a7b7
MOMIN <e.mominbd@gmail.com>
2024-08-14 09:11:23 +0600

    3commint

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
-class homePage extends StatelessWidget {
-  const homePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: AppBar(
-        backgroundColor: Colors.cyanAccent,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(229, 10, 10, 1),
+        title: Text(
+          "eBookOne",
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [],
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.black26,
       ),
     );
   }
